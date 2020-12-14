82da52eafc0ad49a79b899257d2218b9445fb504
golbian <florian13620@hotmail.fr>
2020-12-14 14:05:47 +0100

    EACCESS ERROR fix

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const multer =  require('multer');
-const upload = multer( { dest : '/api/upload' } );
 
 const app = express();
 const db = require("./models");
